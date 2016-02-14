04fb41aa09ad8333a6df2ec082013738addbf88d
MasuqaT <occar@hotmail.co.jp>
2016-02-14 22:06:43 +0900

    Add visual studio debugging process

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var parser = require('body-parser');
 var pg = require('pg');
 var app = express();
 var port = process.env.PORT || 3000;
+var db_con_string = process.env.DATABASE_URL || (function () {
+    var fs = require('fs');
+    var str = fs.readFileSync(__dirname + "/.env", "UTF-8");
+    var str_line = str.split(/\r?\n/).filter(function (v, i, a) { return v.lastIndexOf('DATABASE_URL', 0) == 0; })[0];
+    return str_line.split("=")[1];
+})();
+db_con_string += '?ssl=true';
 app.use(parser.json());
 app.use(express.static(__dirname + '/public'));
 app.get('/api/projects', function (req, res, next) {
--- index.ts
+++ index.ts
@@ -4,7 +4,14 @@ import express = require('express');
 import parser = require('body-parser');
 import pg = require('pg');
 var app = express();
-var port = process.env.PORT || 3000;
+var port: number = process.env.PORT || 3000;
+var db_con_string: string = process.env.DATABASE_URL || (() => { // for Visual Studio debug
+	var fs = require('fs');
+	var str: string = fs.readFileSync(__dirname + "/.env", "UTF-8");
+	var str_line = str.split(/\r?\n/).filter((v, i, a) => v.lastIndexOf('DATABASE_URL', 0) == 0)[0];
+	return str_line.split("=")[1];
+})();
+db_con_string += '?ssl=true';
 
 app.use(parser.json());
 app.use(express.static(__dirname + '/public'));
